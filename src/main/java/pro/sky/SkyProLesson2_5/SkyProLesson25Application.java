637ed8699b1e8b64ec4695ba2856271391a08ff2
Mikhail882 <m.pojidaev@mail.ru>
2022-06-25 02:44:26 +0300

    Initial commit

---
+++ src/main/java/pro/sky/SkyProLesson2_5/SkyProLesson25Application.java
@@ -0,0 +1,15 @@
+package pro.sky.SkyProLesson2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProLesson25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProLesson25Application.class, args);
+
+
+
+	}
+}
